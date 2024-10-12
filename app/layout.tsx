dcff1de240a61b099dc0f72ad1b751343a4db790
Tobias Silicani <silicani80@gmail.com>
2024-10-12 17:37:05 +0200

    Original

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import { AppProvider } from "@toolpad/core/nextjs";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import type { Navigation } from "@toolpad/core";
+
+import theme from "../theme";
+
+const NAVIGATION: Navigation = [
+  {
+    kind: "header",
+    title: "Main items",
+  },
+  {
+    segment: "",
+    title: "Dashboard",
+    icon: <DashboardIcon />,
+  },
+  {
+    segment: "orders",
+    title: "Orders",
+    icon: <ShoppingCartIcon />,
+  },
+];
+
+const BRANDING = {
+  title: "My Toolpad Core Next.js App",
+};
+
+export default function RootLayout(props: { children: React.ReactNode }) {
+  return (
+    <html lang="en" data-toolpad-color-scheme="light" suppressHydrationWarning>
+      <body>
+        <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+          <AppProvider
+            navigation={NAVIGATION}
+            branding={BRANDING}
+            theme={theme}
+          >
+            {props.children}
+          </AppProvider>
+        </AppRouterCacheProvider>
+      </body>
+    </html>
+  );
+}
